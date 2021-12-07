13cf107d388d0db8fb95883bf0b520699dee512a
crimtin <christianmck@hotmail.com>
2021-12-06 22:22:39 -0600

    created input with password, fetched tmdb data on password entry

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,31 @@
 import logo from "./logo.svg";
 import "./App.css";
 import ky from "ky";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
+  const [password, setPassword] = useState("");
+  useEffect(() => {
+    if (password === "grayson") {
+      printMovieData();
+    }
+  }, [password]);
   const printMovieData = async () => {
-    const ditto = await ky
+    const trendingMovieResponse = await ky
       .get(
         "https://api.themoviedb.org/3/trending/movie/week?api_key=" +
           process.env.REACT_APP_TMDB_KEY
       )
       .json();
-    console.log(ditto.results[0].poster_path);
   };
-  printMovieData();
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        {counter}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button type="button" onClick={() => setCounter(counter + 1)}>
-          increment
-        </button>
+        <input
+          type="text"
+          onChange={(event) => setPassword(event.target.value)}
+        />
       </header>
     </div>
   );
